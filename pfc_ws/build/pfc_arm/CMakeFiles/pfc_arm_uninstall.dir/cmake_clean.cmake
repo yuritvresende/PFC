7d5179b6ae073e506a72d5ea24e187d3f55cd40e
Yuri Tavares Varão Resende <yuritvresende@gmail.com>
2024-04-16 10:26:26 -0300

    Criação de arquivos

---
+++ pfc_ws/build/pfc_arm/CMakeFiles/pfc_arm_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pfc_arm_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pfc_arm_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
