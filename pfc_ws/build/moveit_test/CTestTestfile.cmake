d5a4c0c3b097959ddd57a300b440dfebedb2efc4
Yuri Tavares Varão Resende <yuritvresende@gmail.com>
2024-05-07 11:28:19 -0300

    Adicionei dois testes
    Não consigo gerar arquivos de movimento para a garra e para o braço apenas surge o mesmo erro do braço completo do PFC.

---
+++ pfc_ws/build/moveit_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yuritvr/PFC/pfc_ws/src/moveit_test
+# Build directory: /home/yuritvr/PFC/pfc_ws/build/moveit_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
